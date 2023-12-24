c52111e420aec6aacbf309c03d7ee0d3356321a0
Дмитрий Абабков <dmitry@MacBook-Air-Dmitrij.local>
2023-12-25 00:32:55 +0500

    Task 3 ready

--- dist/bundle.js
+++ dist/bundle.js
@@ -16,7 +16,7 @@
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../style.css */ \"./style.css\");\n/* harmony import */ var _img_goblin_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../img/goblin.png */ \"./img/goblin.png\");\n\n\n\ndocument.addEventListener('DOMContentLoaded', () => {\n  const goblin = document.querySelectorAll('.container-row');\n  const arrGoblins = [];\n  const arrToGenerate = [];\n\n  goblin.forEach((item) => arrGoblins.push(item));\n\n  function getRandomInt() {\n    const min = Math.ceil(0);\n    const max = Math.floor(15);\n    return Math.floor(Math.random() * (max - min + 1)) + min;\n  }\n\n  function generationPosition() {\n    const numberGenerate = getRandomInt();\n    if (numberGenerate !== arrToGenerate[arrToGenerate.length]) {\n      arrGoblins.forEach((item) => item.classList.remove('container-goblin'));\n      arrGoblins[numberGenerate].classList.add('container-goblin');\n      arrToGenerate.push(numberGenerate);\n    }\n  }\n\n  generationPosition();\n\n  setInterval(() => {\n    generationPosition();\n  }, 1000);\n});\n\n\n//# sourceURL=webpack://monsters/./js/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../style.css */ \"./style.css\");\n\n// import '../img/goblin.png';\n\ndocument.addEventListener('DOMContentLoaded', () => {\n\n});\n\n\n\n//# sourceURL=webpack://monsters/./js/index.js?");
 
 /***/ }),
 
@@ -27,16 +27,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://monsters/./style.css?");
-
-/***/ }),
-
-/***/ "./img/goblin.png":
-/*!************************!*\
-  !*** ./img/goblin.png ***!
-  \************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-eval("module.exports = __webpack_require__.p + \"2dbd01ce16c0fa83cb67.png\";\n\n//# sourceURL=webpack://monsters/./img/goblin.png?");
 
 /***/ })
 
@@ -67,18 +57,6 @@ eval("module.exports = __webpack_require__.p + \"2dbd01ce16c0fa83cb67.png\";\n\n
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
@@ -88,29 +66,6 @@ eval("module.exports = __webpack_require__.p + \"2dbd01ce16c0fa83cb67.png\";\n\n
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src;
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) {
-/******/ 					var i = scripts.length - 1;
-/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
-/******/ 				}
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /************************************************************************/
--- js/index.js
+++ js/index.js
@@ -1,31 +1,90 @@
 import '../style.css';
-import '../img/goblin.png';
+import db from '../db.json';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const goblin = document.querySelectorAll('.container-row');
-  const arrGoblins = [];
-  const arrToGenerate = [];
+  const trParent = document.querySelector('tr');
+  const tdParent = trParent.querySelectorAll('td');
+  const data = [...db].reverse();
+  const dataSortUpId = [...data].sort((a, b) => a.id - b.id).reverse();
+  const dataSortUpYear = [...data].sort((a, b) => a.year - b.year).reverse();
+  const dataSortUpGrade = [...data].sort((a, b) => a.imdb - b.imdb).reverse();
+  const dataSortDownId = [...data].sort((a, b) => b.id - a.id).reverse();
+  const dataSortDownYear = [...data].sort((a, b) => b.year - a.year).reverse();
+  const dataSortDownGrade = [...data].sort((a, b) => b.imdb - a.imdb).reverse();
 
-  goblin.forEach((item) => arrGoblins.push(item));
+  function renderData(obj) {
+    obj.forEach((item) => {
+      const trCreate = document.createElement('tr');
+      trCreate.setAttribute('data-id', item.id);
+      trCreate.setAttribute('data-title', item.title);
+      trCreate.setAttribute('data-year', item.year);
+      trCreate.setAttribute('data-imdb', item.imdb);
+      trCreate.innerHTML = `
+    <td>${item.id}</td>
+    <td>${item.title}</td>
+    <td>${item.year}</td>
+    <td>imdb: ${item.imdb}</td>`;
+      trParent.after(trCreate);
+    });
+  }
+  renderData(data);
 
-  function getRandomInt() {
-    const min = Math.ceil(0);
-    const max = Math.floor(15);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  function deleteData() {
+    const tr = document.querySelectorAll('tr');
+    tr.forEach((item) => {
+      if (item.dataset.id !== 'id') {
+        item.remove();
+      }
+    });
   }
 
-  function generationPosition() {
-    const numberGenerate = getRandomInt();
-    if (numberGenerate !== arrToGenerate[arrToGenerate.length]) {
-      arrGoblins.forEach((item) => item.classList.remove('container-goblin'));
-      arrGoblins[numberGenerate].classList.add('container-goblin');
-      arrToGenerate.push(numberGenerate);
+  function arrowDefinition(title, arrow, item) {
+    const res = item;
+    res.innerText = title;
+    res.innerText += arrow;
+    return res;
+  }
+
+  let counterString = 0;
+
+  function sortString(dataObj, item, title) {
+    deleteData();
+    if (counterString === 0) {
+      dataObj.sort((a, b) => a.title.localeCompare(b.title)).reverse();
+      renderData(dataObj);
+      arrowDefinition(title, '\u{2193}', item);
+      counterString += 1;
+    } else {
+      dataObj.sort((a, b) => a.title.localeCompare(b.title));
+      renderData(dataObj);
+      arrowDefinition(title, '\u{2191}', item);
+      counterString -= 1;
     }
   }
 
-  generationPosition();
+  let counter = 0;
 
-  setInterval(() => {
-    generationPosition();
-  }, 1000);
+  function sortRendering(dataUp, dataDown, item, title) {
+    deleteData();
+    if (counter === 0) {
+      renderData(dataUp);
+      const arrow = '\u{2193}';
+      arrowDefinition(title, arrow, item);
+      counter += 1;
+    } else {
+      renderData(dataDown);
+      const arrow = '\u{2191}';
+      arrowDefinition(title, arrow, item);
+      counter -= 1;
+    }
+  }
+
+  tdParent.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      if (index === 0) sortRendering(dataSortUpId, dataSortDownId, item, 'id');
+      if (index === 1) sortString(data, item, 'title');
+      if (index === 2) sortRendering(dataSortUpYear, dataSortDownYear, item, 'year');
+      if (index === 3) sortRendering(dataSortUpGrade, dataSortDownGrade, item, 'imdb');
+    });
+  });
 });
